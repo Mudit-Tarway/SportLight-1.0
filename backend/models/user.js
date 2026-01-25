1f495da681d569b244fd9e88c17a48c742b6bb92
Mudit-Tarway <official.mudittarway@gmail.com>
2026-01-25 23:27:41 +0530

    Schema for the user, players and clubs

---
+++ backend/models/user.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  role: { type: String, enum: ["player", "recruiter"] },
+  profileId: { type: mongoose.Schema.Types.ObjectId },
+});
+
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.matchPassword = function (enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+export default User;
