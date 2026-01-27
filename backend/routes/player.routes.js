0d29f702d0c708a5cd34d9f9edfb6dadd3dfc566
Mudit-Tarway <official.mudittarway@gmail.com>
2026-01-27 17:56:42 +0530

    Edited in the player routes

--- backend/routes/player.routes.js
+++ backend/routes/player.routes.js
@@ -1,11 +1,16 @@
 import express from "express";
 import { protect } from "../middleware/auth.middleware.js";
-import { getMyProfile, updatePlayer } from "../controllers/player.controller.js";
+import {
+  getMyProfile,
+  updatePlayer,
+  deleteMyProfile
+} from "../controllers/player.controller.js";
 import { upload } from "../utils/upload.js";
 
 const router = express.Router();
+
 router.get("/me", protect, getMyProfile);
 router.put("/me", protect, upload.single("achievementImage"), updatePlayer);
-
+router.delete("/me", protect, deleteMyProfile); 
 
 export default router;
